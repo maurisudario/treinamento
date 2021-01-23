22c4d9000e104720dbde414fae869d9e4710b8b2
MauriSudario <mauri.sud@gmail.com>
2021-01-23 15:48:27 -0300

    exercico caesb

--- ex_conta_caesb.py
+++ ex_conta_caesb.py
@@ -2,80 +2,92 @@
 # -*- encode: utf-8 -*-
 __author__ = '@MauriSudario'
 
-cliente = input("tipo de cliente?")
+
+cliente=input("tipo de cliente?")
+cliente = {
+    1: residencial_padrao
+    2: residensial_social
+    3: comercial
+    4: paisagismo
+}
+
+consumo=0
+consumo= float(input("Qual seu consumo?"))
+
+
 consumo = 0
 consumo = float(input("Qual seu consumo?"))
 
-if cliente == "residencial padrao":
-    elif consumo = <= 7:
+if cliente == 1:
+    elif consumo <= 7:
     conta = 7 * 2.99
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 8) and ( >= 13):
+    elif consumo  ( <= 8) and ( >= 13):
     conta = 6 * 3.59
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 14) and ( >= 20):
+    elif consumo  ( <= 14) and ( >= 20):
     conta = 7 * 7.10
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 21) and ( >= 30):
+    elif consumo ( <= 21) and ( >= 30):
     conta = 10 * 10.66
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 31) and ( >= 45):
+    elif consumo  ( <= 31) and ( >= 45):
     conta = 15 * 17.05
     print("Sua conta vai ser de: ", conta)
-else consumo = ( >= 46):
+    else consumo  >= 46:
     conta = consumo * 23, 87
     print("Sua conta vai ser de: ", conta)
 
-if cliente == "residencial social":
-    elif consumo = <= 7:
+if cliente == 2:
+    elif consumo  <= 7:
     conta = 7 * 1.49
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 8) and ( >= 13):
+    elif consumo  ( <= 8) and ( >= 13):
     conta = 6 * 1.79
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 14) and ( >= 20):
+    elif consumo ( <= 14) and ( >= 20):
     conta = 7 * 3.55
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 21) and ( >= 30):
+    elif consumo  ( <= 21) and ( >= 30):
     conta = 10 * 5.33
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 31) and ( >= 45):
+    elif consumo  (<= 31) and ( >= 45):
     conta = 15 * 17.05
     print("Sua conta vai ser de: ", conta)
-else consumo = ( >= 46):
+    else consumo (>= 46):
     conta = consumo * 23, 87
     print("Sua conta vai ser de: ", conta)
 
-if cliente == "comercial":
-    elif consumo = <= 4:
+if cliente == 3:
+    elif consumo  <= 4:
     conta = 4 * 6.14
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 5) and ( >= 7):
+    elif consumo ( <= 5) and ( >= 7):
     conta = 3 * 7.68
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 8) and ( >= 10):
+    elif consumo  ( <= 8) and ( >= 10):
     conta = 3 * 9.98
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 11) and ( >= 40):
+    elif consumo  ( <= 11) and ( >= 40):
     conta = 30 * 12.48
     print("Sua conta vai ser de: ", conta)
-else consumo = ( >= 41):
+    else consumo  >= 41:
     conta = consumo * 14.97
     print("Sua conta vai ser de: ", conta)
 
-if cliente == "paisagismo":
-    elif consumo = <= 4:
+if cliente == 4:
+    elif consumo <= 4:
     conta = 4 * 9.21
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 5) and ( >= 7):
+    elif consumo  (<= 5) and ( >= 7):
     conta = 3 * 11.52
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 8) and ( >= 10):
+    elif consumo  (<= 8) and ( >= 10):
     conta = 3 * 14.97
     print("Sua conta vai ser de: ", conta)
-elif consumo = ( <= 11) and ( >= 40):
+    elif consumo ( <= 11) and ( >= 40):
     conta = 30 * 18.72
     print("Sua conta vai ser de: ", conta)
-else consumo = ( >= 41):
+    else consumo  >= 41:
     conta = consumo * 22.46
     print("Sua conta vai ser de: ", conta)
